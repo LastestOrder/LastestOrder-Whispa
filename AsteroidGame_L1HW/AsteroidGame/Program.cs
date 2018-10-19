bfb128f1d470c03e087b1174a6b6791a9a4c3551
LastestOrder <44302374+LastestOrder@users.noreply.github.com>
2018-10-19 21:38:50 +0300

    100

---
+++ AsteroidGame_L1HW/AsteroidGame/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+// Создаем шаблонное потложение, где подключаем модули
+
+namespace AsteroidGame
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Form form = new Form();
+            form.Width = 800;
+            form.Height = 600;
+            SplashScreen.Init(form);
+            form.Show();
+            SplashScreen.Draw();
+
+            //Game.Init(form);
+            //form.Show();
+            //Game.Draw();
+            Application.Run(form);
+        }
+    }
+}
